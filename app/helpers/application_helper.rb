4a736221b336af333c35d141e12e932bc81af93f
Arnoldo Rodriguez Colin <acolin@incaztech.com>
2012-02-04 13:34:08 -0600

    quick push

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,16 +12,16 @@ module ApplicationHelper
         if visitor.id == fellowship.inviter_id
           links << "Request Already Sent"
         elsif visitor.id == fellowship.invited_id
-          links << fellowship_update_link fellowship, visited, Fellowship::STATUSES[:working]
-          links << fellowship_update_link fellowship, visited, Fellowship::STATUSES[:rejected]
+          links << fellowship_update_link(fellowship, visited, Fellowship::STATUSES[:working])
+          links << fellowship_update_link(fellowship, visited, Fellowship::STATUSES[:rejected])
         end
       when fellowship.working?
           links << "I'm your Coworker"
-          links << fellowship_update_link fellowship, visited, Fellowship:: STATUSES[:worked]
+          links << fellowship_update_link(fellowship, visited, Fellowship::STATUSES[:worked])
 
       when fellowship.worked?
           links << "You've worked with me"
-          links << fellowship_update_link fellowship, visited, Fellowship::STATUSES[:pending]
+          links << fellowship_update_link(fellowship, visited, Fellowship::STATUSES[:pending])
       end
       links.html_safe
     end
